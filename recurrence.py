834c04c5c9b3288bfe970eaba145cac174a90edb
Buffalo Hird <buffalohird@college.harvard.edu>
2014-02-10 11:07:57 -0500

    about to really change i

--- recurrence.py
+++ recurrence.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-import time
 import copy
 
 def solve_recurrences(line_1, line_2, line_3):
@@ -8,6 +6,10 @@ def solve_recurrences(line_1, line_2, line_3):
 	K, N = map(int, line_1.split())
 	a = map(int, line_2.split())
 	c = map(int, line_3.split())
+
+	if(N < 5):
+		i = len(a) - 1
+		return solve_helper_old(K, N, a, c, i)
 
 	return solve_helper(K, N, a, c)
 
@@ -28,20 +30,45 @@ def solve_helper(K, N, a, c):
 
 	matrix_a = [a[::-1]]
 	old_matrix = copy.copy(matrix)
+	matrix_list = []
+	matrix_bool = False
 	while i < N:
 		if i * 2 < N:
 			matrix = matrix_multiply(matrix, matrix)
 			i += i - 1
-
-			for item in matrix:
-				print item
-			#print " "
+			matrix_list.append(matrix)
 		else:
-			matrix = matrix_multiply(old_matrix, matrix)
-			i += 1
+			"""
+			for item in reversed(matrix_list):
+				print matrix_list
+				index = matrix_list.index(item) + 1
+				matrix_bool = False
+				if i + 2 ** index < N:
+					matrix = matrix_multiply(matrix, item)
+					i += index
+					matrix_bool = True
+					break"""
+
+			if matrix_bool == False:
+				matrix = matrix_multiply(old_matrix, matrix)
+				i += 1
+
+		print i 
+		
+		print ""
 
 
 	return matrix_multiply(matrix, matrix_a)[0][0] % 1000
+
+
+def solve_helper_old(K, N, a, c, i):
+	if i == N:
+		return a.pop() % 1000
+
+	new_result_list = zip(reversed(a), c)
+	new_result = reduce(lambda sum, (x, y): sum + x*y, new_result_list, 0)
+	a.append(new_result)
+	return solve_helper_old(K, N, a, c, i+1)
 
 
 def matrix_multiply(a,b):
@@ -53,31 +80,17 @@ def matrix_multiply(a,b):
 
 
 
-"""
-	new_result_list = zip(reversed(a), c)
-	new_result = reduce(lambda sum, (x, y): sum + x*y, new_result_list, 0)
-	a.append(new_result)
-	return solve_helper(K, N, a, c, i+1)
-"""
-
-
-
-
-
 line_1 = "2 100"
 line_2 = "12 17"
 line_3 = "9 15"
-
 """
 line_1 = raw_input()
 line_2 = raw_input()
 line_3 = raw_input()
 """
 
-startTime = datetime.now()
+
 print solve_recurrences(line_1, line_2, line_3)
-endTime = datetime.now()
-#print (endTime - startTime).total_seconds()
 
 
 
