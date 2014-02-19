1698d5124077f0f26670c4327e2c3fc6e6e1ea28
Buffalo Hird <buffalohird@college.harvard.edu>
2014-02-19 11:51:20 -0500

    some updates to keys and gates

--- maze.py
+++ maze.py
@@ -19,8 +19,6 @@ class Search:
 		"""
 		
 		
-		
-		
 		self.goalStates = []
 		keys = []
 		gates = []
@@ -38,62 +36,56 @@ class Search:
 		keys.sort(key=lambda item: item[0])
 		gates.sort(key=lambda item: item[0])
 
+		for key in keys:
+			self.goalStates.append(key[1])
 
-		states = [j for i in zip(keys, gates) for j in i]
-		for item in states:
-			self.goalStates.append(item[1])
+		#states = [j for i in zip(keys, gates) for j in i]
+		#for item in states:
+			#self.goalStates.append(item[1])
 		self.goalStates.append(self.goalPosition)
 
-
-		inputNode = [self.agentPosition, 0, self.T]
+		inputNode = [0, self.agentPosition, self.T, []]
 		for goal in self.goalStates:
 			inputNode = self.makeSearch(inputNode, goal)
 			j, i = goal
-			self.grid[i][j] = "O"
+		
 			"""
 			print "\n"
 			print "returned node from iteration"
 			print inputNode
 			print "\n"
 			"""
+			
 
-
-			#for item in self.grid:
-				#print item
-
-
-		return inputNode[1]
-
-
-
-		#print self.agentPosition
+		return inputNode[0]
 
 	def makeSearch(self, inputNode, goal):
 		visited = []
 		node = inputNode
 		fringe = Queue.PriorityQueue()
-		while not node[0] == goal:
-			next = self.getSuccessors(node[0], goal)
+		while not node[1] == goal:
+			#print node
+			next = self.getSuccessors(node[1], goal, inputNode[3])
 
 			for item in next:
 				newT = node[2] + item[1]
 				if newT > 0:
-					fringe.put([item[0], node[1] + 1, newT])
+					fringe.put([node[0] + 1, item[0], newT, item[2]])
 				#else:
 					#visited.append(item[0])
 			newBool = False
 			while not newBool:
 				if fringe.empty():
-					return [None, -1, None]
+					return [-1, None, None]
 				item = fringe.get()
-				if item[0] not in visited:
+				if item[1] not in visited:
 					newBool = True
 			node = item
-			visited.append(item[0])
+			visited.append(item[1])
 
 		return node
 
-	def getSuccessors(self, position, goal):
+	def getSuccessors(self, position, goal, currentKeys):
 		if position == None:
 			return []
 		x,y = position
@@ -103,9 +95,23 @@ class Search:
 			if j > self.M - 1 or i > self.N -1 or j < 0 or i < 0:
 				continue
 			if self.grid[i][j] == "O" or self.grid[i][j] == "E" or self.grid[i][j] == "S" or (j, i) == goal:
-				successors.append([(j, i), 0])
+				successors.append([(j, i), 0, currentKeys])
 			if self.grid[i][j] == "M":
-				successors.append([(j, i), -1])
+				successors.append([(j, i), -1, currentKeys])
+			if self.grid[i][j][0] == "G":
+				for key in currentKeys:
+					if self.grid[i][j][1] == key[0][1]:
+						successors.append([(j, i), 0, currentKeys])
+			if self.grid[i][j][0] == "K":
+				foundKeyBool = False
+				for key in currentKeys:
+					if self.grid[i][j][1] == key[0][1]:
+						successors.append([(j, i), 0, currentKeys])
+						foundKeyBool = True
+				if foundKeyBool == False:
+					currentKeys.append([self.grid[i][j], (j, i)])
+					successors.append([(j, i), 0, currentKeys])
+
 
 		"""
 		print "###state %s, %s has succesors:" % (str(x), str(y))
@@ -116,21 +122,12 @@ class Search:
 
 		return successors
 
-
-
-
-
-
-
-
-
-
 input1 = ["4 4 1", "X O O E", "O O X O", "O X X O", "S O O X"]
 input2 = ["5 4 3", "M O O E", "O X X M", "O X X M", "O X X M", "M O O S"]
 input3 = ["4 4 2", "O O O E", "M X O M", "M X O M", "O O O S"]
 input4 = ["4 4 2", "O K1 G2 E", "O O X G1", "O S O O", "O O X K2"]
 input5 = ["4 4 1", "X O O E", "X X X X", "O O O O", "S O O O"]
 newSearch = Search()
-print newSearch.solve(input2)
+print newSearch.solve(input4)
 
 
