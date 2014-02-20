43f8c1d3635b8418e261f1cae0065f70db706ac9
Buffalo Hird <buffalohird@college.harvard.edu>
2014-02-20 03:39:44 -0500

    correct 93

--- maze.py
+++ maze.py
@@ -1,6 +1,5 @@
 import Queue
 import time
-import copy
 
 class Search:
 
@@ -10,98 +9,124 @@ class Search:
 		
 		
 		self.N, self.M, self.T = map(int, raw_input().split())
-		startFoundBool = False
 		for item in xrange(self.N):
-			inputRow = map(str, raw_input().split())
-			self.grid.append(inputRow)
-			if not startFoundBool and 'S' in inputRow:
-				startFoundBool = True
-				self.agentPosition = (inputRow.index('S'), item)
+			self.grid.append(map(str, raw_input().split()))
 		"""
 		self.N, self.M, self.T = map(int, input[0].split())
-		startFoundBool = False
 		for i in xrange(self.N):
-			inputRow = map(str, input[i+1].split())
-			self.grid.append(inputRow)
-			if not startFoundBool and 'S' in inputRow:
-				startFoundBool = True
-				self.agentPosition = (inputRow.index('S'), i)
+			self.grid.append(map(str, input[i+1].split()))
 		"""
-
 		
 		
 		
-	
+		self.goalStates = []
+		keys = []
+		gates = []
+		for item in self.grid:
+			for state in item:
+				if state == "S":
+					self.agentPosition = (item.index(state), self.grid.index(item))
+				if state == "E":
+					self.goalPosition = (item.index(state), self.grid.index(item))
+				if state[0] == "K":
+					keys.append([str(state[1]), (item.index(state), self.grid.index(item))])
+				if state[0] == "G":
+					gates.append([str(state[1]), (item.index(state), self.grid.index(item))])
 
-		inputNode = [0, self.agentPosition, self.T, ""]
-		return self.makeSearch(inputNode)[0]
+		keys.sort(key=lambda item: item[0])
+		gates.sort(key=lambda item: item[0])
 
-	def makeSearch(self, inputNode):
-		j, i = inputNode[1]
-		if self.grid[i][j] == "E":
-			return inputNode
-		visited = set()
-		visited.add((inputNode[1], inputNode[2], inputNode[3]))
+		for key in keys:
+			self.goalStates.append(key[1])
+
+		#states = [j for i in zip(keys, gates) for j in i]
+		#for item in states:
+			#self.goalStates.append(item[1])
+		self.goalStates.append(self.goalPosition)
+
+		inputNode = [0, self.agentPosition, self.T, []]
+		for goal in self.goalStates:
+			inputNode = self.makeSearch(inputNode, goal)
+			j, i = goal
+		
+			"""
+			print "\n"
+			print "returned node from iteration"
+			print inputNode
+			print "\n"
+			"""
+			
+
+		return inputNode[0]
+
+	def makeSearch(self, inputNode, goal):
+		visited = []
 		node = inputNode
-		fringe = Queue.Queue()
-		while True:
-			next = self.getSuccessors(node)
+		fringe = Queue.PriorityQueue()
+		while not node[1] == goal:
+			#print node
+			next = self.getSuccessors(node[1], goal, inputNode[3])
+
 			for item in next:
-				j,i = item[1]
-				if self.grid[i][j] == "E":
-					return item
-				if (item[1], item[2], item[3]) not in visited:
-					fringe.put(item)
+				newT = node[2] + item[1]
+				if newT > 0:
+					fringe.put([node[0] + 1, item[0], newT, item[2]])
+				else:
+					visited.append(item[1])
+			newBool = False
+			while not newBool:
+				if fringe.empty():
+					return [-1, None, None, None]
+				item = fringe.get()
+				if item[1] not in visited:
+					newBool = True
+			node = item
+			visited.append(item[1])
 
-			while node[2] > 0:
-				visited.add((node[1], node[2], node[3]))
-				node[2] -= 1
-			if fringe.empty():
-				return [-1, None, None, None]
-			else:
-				node = fringe.get()
+		return node
 
-
-	def getSuccessors(self, node):
-		x,y = node[1]
+	def getSuccessors(self, position, goal, currentKeys):
+		if position == None:
+			return []
+		x,y = position
 		potentials = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
 		successors = []
 		for (j, i) in potentials:
 			if j > self.M - 1 or i > self.N -1 or j < 0 or i < 0:
 				continue
-			potentialName = self.grid[i][j]
-			if potentialName == "X":
-				continue
-			elif potentialName == "E":
-				return [[node[0] + 1, (j, i), node[2], node[3]]]
-			elif potentialName == "O" or potentialName == "S":
-				successors.append([node[0] + 1, (j, i), node[2], node[3]])
-			elif potentialName == "M":
-				if node[2] - 1 != 0:
-					successors.append([node[0] + 1, (j, i), node[2] - 1, node[3]])
-			
-			elif potentialName[0] == "G":
-				for key in node[3]:
-					if self.grid[i][j][1] == key:
-						successors.append([node[0] + 1, (j, i), node[2], node[3]])
-
-			elif potentialName[0] == "K":
+			if self.grid[i][j] == "O" or self.grid[i][j] == "E" or self.grid[i][j] == "S" or (j, i) == goal:
+				successors.append([(j, i), 0, currentKeys])
+			if self.grid[i][j] == "M":
+				successors.append([(j, i), -1, currentKeys])
+			if self.grid[i][j][0] == "G":
+				for key in currentKeys:
+					if self.grid[i][j][1] == key[0][1]:
+						successors.append([(j, i), 0, currentKeys])
+			if self.grid[i][j][0] == "K":
 				foundKeyBool = False
-				for key in node[3]:
-					if potentialName[1] == key:
-						successors.append([node[0] + 1, (j, i), node[2], node[3]])
-						continue
+				for key in currentKeys:
+					if self.grid[i][j][1] == key[0][1]:
+						successors.append([(j, i), 0, currentKeys])
+						foundKeyBool = True
 				if foundKeyBool == False:
-					successors.append([node[0] + 1, (j, i), node[2], str(sorted(node[3][:] + potentialName[1]))])
-			
-		
+					currentKeys.append([self.grid[i][j], (j, i)])
+					successors.append([(j, i), 0, currentKeys])
+
+
+		"""
+		print "###state %s, %s has succesors:" % (str(x), str(y))
+		for item in successors:
+			print item
+		print "\n"
+		"""
 
 		return successors
 
 input1 = ["4 4 1", "X O O E", "O O X O", "O X X O", "S O O X"]
 input2 = ["5 4 3", "M O O E", "O X X M", "O X X M", "O X X M", "M O O S"]
 input3 = ["4 4 2", "O O O E", "M X O M", "M X O M", "O O O S"]
-input4 = ["4 4 2", "O K1 G2 E", "M M X G1", "O S O O", "O O X K2"]
+input4 = ["4 4 2", "O K2 G2 E", "O O X G1", "O S O O", "O O X K1"]
 input5 = ["4 4 1", "X O O E", "X X X X", "O O O O", "S O O O"]
 newSearch = Search()
 print newSearch.solve(input4)
+
