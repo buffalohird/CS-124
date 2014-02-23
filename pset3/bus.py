64747e1c46fd75fe13546130f9a6a8f43d384ecb
Buffalo Hird <buffalohird@college.harvard.edu>
2014-02-23 15:21:54 -0500

    so close

--- pset3/bus.py
+++ pset3/bus.py
@@ -2,54 +2,37 @@ import Queue
 
 class moneyBus:
 
-	def calcPathCost(self, vertex1, vertex2, pathCost):
-		return pathCost - self.P[vertex2]
+	def calcPathCost(self, edge):
+		return edge[2] - self.P[edge[1]]
 
 	def solve(self):
 		self.N, self.E = map(int, raw_input().split())
-		self.edges = [[float("inf") for _ in xrange(self.N)] for _ in xrange(self.E)]
+		self.edges = []
 		self.P = map(int, raw_input().split())
 		for item in xrange(self.E):
 			i, j, k = map(int, raw_input().split())
-			self.edges[i - 1][j - 1] = k
+			self.edges.append([i - 1, j - 1, k])
 
 		# initialize graph
 		distance = [float("inf") for _ in xrange(self.N)]
 		predecessor = [None for _ in xrange(self.N)]
-		distance[0] = 0
+		distance[0] = 0 - self.P[0]
 
 		for vertex in xrange(self.N):
 			for edge in self.edges:
-				if distance[edge[0]]
+				newPathCost = distance[edge[0]] + self.calcPathCost(edge)
+				if distance[edge[0]] + self.calcPathCost(edge) < distance[edge[1]]:
+					distance[edge[1]] = newPathCost
+					predecessor[edge[1]] = edge[0]
 
+		if distance[self.N - 1] == float("inf"):
+			return None
 
-		
+		for edge in self.edges:
+			if distance[edge[0]] + self.calcPathCost(edge) < distance[edge[1]]:
+				return "infinity"
 
-		#currentPosition = [0, 1]
-		"""
-		for vertex in xrange(self.N):
-			shortestPaths[vertex][vertex] = 0
-			outnodes = self.edges[vertex]
-			for item in outnodes:
-				if item != float("inf"):
-					itemIndex = outnodes.index(item)
-					totalWeight = self.calcPathCost(vertex, itemIndex, item)
-					shortestPaths[vertex][itemIndex] = totalWeight
-
-		for k in xrange(self.N):
-			for i in xrange(self.N):
-				for j in xrange(self.N):
-					if shortestPaths[i][j] > shortestPaths[i][k] + shortestPaths[k][j]:
-						shortestPaths[i][j] = shortestPaths[i][k] + shortestPaths[k][j]
-
-		for item in shortestPaths:
-			print item
-		"""
-			
-			
-			
-		return 0
-
+		return -1 * distance[self.N - 1]
 
 
 problem = moneyBus()
