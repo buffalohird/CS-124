0bdb6698fdbd067bb42819537c657f25a5e8be48
Buffalo Hird <buffalohird@college.harvard.edu>
2014-03-03 17:59:24 -0500

    about to change

--- pset4/intervals.py
+++ pset4/intervals.py
@@ -8,16 +8,27 @@ class Intervals:
 		self.b = map(int, raw_input().split())
 
 		newArray = sorted(zip(self.a,self.b))
-		arrayEnd = max(self.b)
-		#arrayStart = max(self.a)
-		values = [0] * arrayEnd
+		maxCounter = 0
+		counter = 0
+		aCounter = 0
+		bCounter = 0
+		while True:
+			a = newArray[aCounter][0]
+			b = newArray[bCounter][1]
+			previousB = newArray[bCounter - 1][1]
+			#print a, b, counter
+			if a < b:
+				counter += 1
+				aCounter += 1
+			else:
+				maxCounter = max(maxCounter, counter)
+				aCounter -= counter
+				counter = 0
+				bCounter += 1
 
-		for (a,b) in newArray:
-			print a, b
-			for i in xrange(b - a):
-				values[a + i] += 1
+			if aCounter >= self.k or bCounter >= self.k:
+				return maxCounter
 
-		return max(values)
 
 
 
