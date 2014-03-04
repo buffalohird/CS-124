6af58291918158ac3005c398de3a17a1e24c9d32
Buffalo Hird <buffalohird@college.harvard.edu>
2014-03-03 23:50:27 -0500

    super push

--- pset4/intervals.py
+++ pset4/intervals.py
@@ -6,17 +6,14 @@ class Intervals:
 		self.k = int(raw_input())
 		self.a = map(lambda x: [int(x), 1], raw_input().split())
 		self.b = map(lambda y: [int(y), -1], raw_input().split())
-
 		values = [0] * self.k * 2
-
 		newList = sorted(self.a + self.b)
 		newListLength = len(newList)
-		for index in xrange(newListLength):
+		print newList
+
+		for index in xrange(newListLength / 2 + 5):
 			values[index] += newList[index][1] + values[index - 1]
-
 		return max(values)
-
-
 
 problem = Intervals()
 print problem.solve()
