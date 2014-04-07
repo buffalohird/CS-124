1b0efa057feac10251de473cc8a480ca02d79a7d
Buffalo Hird <buffalohird@college.harvard.edu>
2014-04-07 02:19:02 -0400

    some progress f the police

--- pset7/gap.py
+++ pset7/gap.py
@@ -1,17 +1,55 @@
-
+import operator
 
 
 class Solution:
 
 	def solve(self):
 		self.K = int(raw_input())
-		self.A = map(int, raw_input().split())
-		self.B = map(int, raw_input().split())
-		self.C = map(int, raw_input().split())
-		self.D = map(int, raw_input().split())
+		intervals = []
+
+
+
+		for _ in xrange(4):
+			intervals.append(map(int, raw_input().split()))
 
 
 		# sort these fuckers
+		intervals = zip(*intervals)
+		
+
+		intervals = sorted(intervals, key=lambda x: x[3])
+
+		
+		index = 1
+		intervalsLength = len(intervals)
+		newIntervals = []
+		print intervals
+		item = [intervals[0][0], intervals[0][3], 1]
+		while index < intervalsLength:
+			print item
+			if intervals[index][0] >= item[1]:
+				print "hello"
+				item[2] += 1
+				item[1] = intervals[index][3]
+				index += 1
+			else:
+				print "bye"
+				index += 1
+				if index < intervalsLength:
+					newIntervals.append(item)
+					item = [intervals[index][0], intervals[index][3], 1]
+
+
+		newIntervals.append(item)
+
+		print newIntervals
+
+
+
+
+			
+
+	
 
 
 		# merge intervals and do weights instead
